a12f582da8f6ef859e266212281ad02cbc89e60f
haubibi2007 <349274297@qq.com>
2019-09-09 21:12:45 +0800

    rect

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hithere!</div>;
+};
+
+console.log(1);
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
